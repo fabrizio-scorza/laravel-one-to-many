2ccc92b011739878ee4e3fafe713bc8c8362d9cb
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-06-05 16:10:40 +0200

    FIX: project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,6 +16,9 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        $types = Type::all();
+        $ids = $types->pluck('id')->all();
+
         //
         for ($i = 0; $i < 10; $i++) {
 
@@ -26,7 +30,7 @@ class ProjectSeeder extends Seeder
             $project->slug = Str::slug($name);
             $project->description = $faker->optional()->text(250);
             $project->link = $faker->url();
-
+            $project->type_id = $faker->randomElement($ids);
             $project->save();
         }
     }

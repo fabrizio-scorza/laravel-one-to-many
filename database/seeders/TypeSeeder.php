b60d3dbff14e011075db2ccb6dc20f6545d5121e
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-06-05 15:42:34 +0200

    ADD: type seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -13,5 +16,15 @@ class TypeSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('categories')->truncate();
+        $types = ['HTML-CSS', 'JS', 'Vue', 'Vite', 'PHP', 'Laravel'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($type);
+
+            $new_type->save();
+        }
     }
 }
